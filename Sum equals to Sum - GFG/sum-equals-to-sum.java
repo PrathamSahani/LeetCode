bbf542b1feed13ae2f064f022bd6fed4c47dc727
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-07 21:07:12 +0530

    Added solution - LeetHub

--- Sum equals to Sum - GFG/sum-equals-to-sum.java
+++ Sum equals to Sum - GFG/sum-equals-to-sum.java
@@ -40,18 +40,17 @@ class Solution
 { 
     public int findPairs(int arr[],int n) 
     {    
-        HashSet<Integer> hs = new HashSet<>();
-        int count =0;
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
-                if(hs.contains(arr[i]+arr[j])){
-                    return 1;
-                }else{
-                    hs.add(arr[i]+arr[j]);
-                }
-            }
-        }
-        return 0;
+       HashSet<Integer> hs = new HashSet<>();
+       for(int i=0; i<arr.length; i++){
+           for(int j=i+1; j<arr.length; j++){
+               if(hs.contains(arr[i]+arr[j])){
+                   return 1;
+               }else{
+                   hs.add(arr[i]+arr[j]);
+               }
+           }
+       }
+       return 0;
         
     }
 }
