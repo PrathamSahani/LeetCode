86b76ac586e4c841b5bf12375b40435f8e1ee640
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-12 22:39:55 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.java
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -45,16 +45,17 @@ class Solution {
         vis[0] = true;
         
         while(!q.isEmpty()){
-            Integer node = q.poll();
-            bfs.add(node);
-            
-            for(Integer it: adj.get(node)){
-                if(vis[it]==false){
-                    vis[it] = true;
-                    q.add(it);
-                }
-            }
+           Integer node = q.poll();
+           bfs.add(node);
+           
+          for(Integer it: adj.get(node)){
+              if(vis[it]==false){
+                  vis[it] = true;
+                  q.add(it);
+              }
+          }
         }
-        return bfs;
+        
+      return bfs;
     }
 }
