e7bec9863c84c5befb127717cc198239b3cb4889
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-13 09:11:19 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[] = new boolean[V];
+        for(int i=0; i<V; i++){
+            if(visited[i]==false){
+                if(DFS(i, -1, visited, adj))
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean DFS(int src, int parent, boolean visited[], ArrayList<ArrayList<Integer>> adj){
+        visited[src] = true;
+        for(int it:adj.get(src)){
+            if(visited[it]==false){
+            if(DFS(it, src, visited, adj))
+            return true;
+            }
+        
+        else if(it!=parent){
+            return true;
+        }
+    }
+    return false;
+  
+    }
+
+}
