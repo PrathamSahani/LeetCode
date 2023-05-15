d88f545415a36c6b1c6a65d7f30fa090031f7d8d
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-15 21:13:10 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -46,15 +46,15 @@ class GFG {
 
 class Solution {
     public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+          ArrayList<Long> list = new ArrayList<>();
+          for(int i=0; i<arr1.length; i++){
+              list.add((long)arr1[i]);
+          }
+          for(int i=0; i<arr2.length; i++){
+              list.add((long)arr2[i]);
+          }
+          Collections.sort(list);
+          return list.get(k-1);
         
-           ArrayList<Integer> list = new ArrayList<>();
-           for(int i=0; i<n; i++){
-               list.add(arr1[i]);
-           }
-           for(int i=0; i<m; i++){
-               list.add(arr2[i]);
-           }
-           Collections.sort(list);
-           return list.get(k-1);
     }
 }
