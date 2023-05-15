2683ffab6b6877ab19a17d673370e7233c2d8f32
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-15 21:42:54 +0530

    Added solution - LeetHub

---
+++ Binary Search in forest - GFG/binary-search-in-forest.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GfG
+{
+    public static void main (String[] args)
+	{
+        Scanner in = new Scanner(System.in);
+        
+        int t = in.nextInt();
+        while(t-->0)
+        {
+            int n = in.nextInt();
+            
+            int tree[]= new int[n];
+            for(int i=0; i<n; i++)
+                tree[i] = in.nextInt();
+            int k = in.nextInt();
+            
+            Solution x = new Solution();
+            System.out.println( x.find_height(tree,n,k) );
+        }
+	}
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    static int find_height(int tree[], int n, int k)
+    {
+      
+       int low =0;
+       Arrays.sort(tree);
+       while(low<tree[n-1]){
+           int sum =0;
+           for(int i=0; i<n; i++){
+               if(tree[i]>low){
+                   sum+=tree[i]-low;
+               }
+           }
+           if(sum==k){
+               return low;
+           }
+           low++;
+       }
+       return -1;
+        
+    }
+}
