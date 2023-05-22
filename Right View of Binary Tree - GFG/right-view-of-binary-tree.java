db993d06068a8e1d29acc07e6a5f48cf56ac149e
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-22 21:05:39 +0530

    Added solution - LeetHub

--- Right View of Binary Tree - GFG/right-view-of-binary-tree.java
+++ Right View of Binary Tree - GFG/right-view-of-binary-tree.java
@@ -132,40 +132,28 @@ class Solution{
     //Function to return list containing elements of right view of binary tree.
     ArrayList<Integer> rightView(Node root) {
         //add code here.
-        ArrayList<Integer> list = new ArrayList<>();
-        if(root==null)return list;
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-             int size = q.size();
-             for(int i=0; i<size; i++){
-                 Node temp = q.poll();
-                 if(i==size-1){
-                     list.add(temp.data);
-                 }
-                 if(temp.left!=null){
-                 q.add(temp.left);}
-                 if(temp.right!=null){
-                 q.add(temp.right);}
-             }
-        }
-        return list; 
+          ArrayList<Integer> list = new ArrayList<>();
+            if(root==null)return list;
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            while(!q.isEmpty()){
+               
+                int size = q.size();
+                for(int i=0; i<size; i++){
+                    Node temp = q.poll();
+                    if(i==size-1)
+                    list.add(temp.data);
+                    if(temp.left!=null){
+                        q.add(temp.left);
+                    }
+                    if(temp.right!=null){
+                        q.add(temp.right);
+                    }
+
+                }
+                
+            }
+            return list;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
