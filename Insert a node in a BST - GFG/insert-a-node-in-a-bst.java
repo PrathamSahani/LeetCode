3585acafbb1f2cdb38615ca9758fa78752cb36bb
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-23 19:26:13 +0530

    Added solution - LeetHub

--- Insert a node in a BST - GFG/insert-a-node-in-a-bst.java
+++ Insert a node in a BST - GFG/insert-a-node-in-a-bst.java
@@ -106,17 +106,17 @@ class Solution {
     // Function to insert a node in a BST.
     Node insert(Node root, int key) {
         // your code here
-        if(root==null){
-            root = new Node(key);
-            return root;
-        }
-        else if(key<root.data){
-            root.left = insert(root.left, key);
-        }
-        else if(key>root.data){
-            root.right = insert(root.right, key);
-          
-        }  return root;
+       if(root==null){
+           root = new Node(key);
+           return root;
+       }
+       if(key>root.data){
+           root.right = insert(root.right, key);
+       }
+      if(key<root.data){
+          root.left = insert(root.left, key);
+      }
+      return root;
      
     }
 }
