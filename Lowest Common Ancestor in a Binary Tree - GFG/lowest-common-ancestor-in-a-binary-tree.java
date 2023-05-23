df8e7edf5865baf97e9ea184017d6133ac5a5516
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-23 11:51:00 +0530

    Added solution - LeetHub

--- Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.java
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.java
@@ -119,60 +119,23 @@ class Node
     }
 }*/
 
-
-/* A Binary Tree node
-class Node
-{
-    int data;
-    Node left, right;
-
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
-    }
-}*/
-
-
-
-/* A Binary Tree node
-class Node
-{
-    int data;
-    Node left, right;
-
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
-    }
-}*/
-
 class Solution
 {
     //Function to return the lowest common ancestor in a Binary Tree.
 	Node lca(Node root, int n1,int n2)
 	{
 		// Your code here
-	if(root==null)return null;
-	if(root.data==n1 || root.data==n2)
-	{return root;}
-	Node left = lca(root.left, n1, n2);
-	Node right = lca(root.right, n1, n2);
-	if(right==null)return left;
-else 	if(left==null)return right;
-	return root;
-		
+		if(root==null)return null;
+		if(root.data==n1 || root.data==n2){
+		    return root;
+		}
+		Node left = lca(root.left, n1, n2);
+		Node right = lca(root.right, n1, n2);
+		if(right==null)return left;
+	else 	if(left==null)return right;
+		return root;
 	}
 }
-
-
-
-
-
-
-
-
 
 
 
