19b5df44edd257a2b2abf88bb234f45b07554036
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-05-26 18:27:31 +0530

    Added solution - LeetHub

--- DFS of Graph - GFG/dfs-of-graph.java
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -38,7 +38,6 @@ class Solution {
     public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ls){
         vis[node] = true;
         ls.add(node);
-        
         for(Integer it: adj.get(node)){
             if(vis[it]==false){
                 dfs(it, vis, adj, ls);
@@ -46,13 +45,12 @@ class Solution {
         }
     }
     // Function to return a list containing the DFS traversal of the graph.
-    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){   
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean vis[] = new boolean[V+1];
         vis[0] = true;
         ArrayList<Integer> ls = new ArrayList<>();
         dfs(0, vis, adj, ls);
         return ls;
-        
     }
 }
