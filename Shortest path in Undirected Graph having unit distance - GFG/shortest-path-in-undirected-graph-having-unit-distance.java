e39fbcff3bf70172b4e25de6b00014dca7da4c3f
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-06 20:12:45 +0530

    Added solution - LeetHub

--- Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -42,26 +42,38 @@ class Solution {
             adj.get(edges[i][1]).add(edges[i][0]);
         }
         int dist[] = new int[n];
-        for(int i=0; i<n; i++)
-        dist[i] = (int)1e9;
-        dist[src] =0;
+        for(int i=0; i<n; i++)dist[i]= (int)1e9;
+        dist[src] = 0;
         Queue<Integer> q = new LinkedList<>();
         q.add(src);
         while(!q.isEmpty()){
             int node = q.peek();
             q.remove();
-            for(int it: adj.get(node)){
+            for(int it:adj.get(node)){
                 if(dist[node] +1<dist[it]){
-                    dist[it] = 1+dist[node];
+                    dist[it]=1+dist[node];
                     q.add(it);
                 }
             }
         }
         for(int i=0; i<n; i++){
             if(dist[i]==1e9){
-                dist[i] =-1;
+                dist[i] = -1;
             }
         }
         return dist;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
