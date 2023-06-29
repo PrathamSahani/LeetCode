38b6881e15352eb6d3e7eb6cbd98a1c189b87e83
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-29 14:57:56 +0530

    Time: 0 ms (100%), Space: 44 MB (10.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,6 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-         int low = 0, high = nums.length-1;
+         int low = 0 , high = nums.length-1;
          while(low<=high){
              int mid = low+(high-low)/2;
              if(nums[mid]==target){
@@ -12,6 +12,5 @@ class Solution {
              }
          }
          return low;
-
     }
 }
