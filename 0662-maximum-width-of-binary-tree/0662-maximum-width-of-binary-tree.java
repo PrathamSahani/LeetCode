e034d3b207171761c3637b08d5d4ba5ff6ca3dec
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-03 16:25:24 +0530

    Time: 1 ms (100%), Space: 43.6 MB (13.3%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -17,36 +17,35 @@
      int pos;
      TreeNode node;
      Pair(int x, TreeNode y){
-        pos = x;
-        node = y;
+         pos = x;
+         node = y;
      }
  }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, root));
-        int max = Integer.MIN_VALUE;
-        while(!q.isEmpty()){
-            int n = q.size();
-            int start =0, end =0;
-            for(int i=0; i<n; i++){
-                Pair p = q.poll();
-                int pos = p.pos;
-                TreeNode node = p.node;
-                if(i==0){
-                    start = pos;
-                }if(i==n-1){
-                    end = pos;
-                }
-                if(node.left!=null){
-                    q.add(new Pair(2*pos+1, node.left));
-                }if(node.right!=null){
-                    q.add(new Pair(2*pos+2, node.right));
-                }
-            }
-            max = Math.max(max , end+1-start);
-        }
-        return max;
+       Queue<Pair> q = new LinkedList<>();
+       q.add(new Pair(0, root));int max= Integer.MIN_VALUE;
+       while(!q.isEmpty()){
+           int n = q.size();
+           int start =0, end =0;
+           for(int i=0; i<n; i++){
+               Pair p = q.poll();
+               int pos = p.pos;
+               TreeNode node = p.node;
+               if(i==0){
+                   start = pos;
+               }if(i==n-1){
+                   end = pos;
+               }
+               if(node.left!=null){
+                   q.add(new Pair(2*pos+1, node.left));
+               }if( node.right!=null){
+                   q.add(new Pair(2*pos+2, node.right));
+               }
+           }
+           max = Math.max(max, end+1-start);
+       }
+       return max;
     }
 
 }
