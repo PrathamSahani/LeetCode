de3ec5822488eb2771b78d4832bf6df26cc31795
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-10 20:50:13 +0530

    Time: 16 ms (75.45%), Space: 51.3 MB (74.37%) - LeetHub

---
+++ 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] gardenNoAdj(int n, int[][] paths) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int ans[] = new int[n];
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(var p: paths){
+            adj.get(p[0]-1).add(p[1]-1);
+            adj.get(p[1]-1).add(p[0]-1);
+        }
+        for(int i=0; i<n; i++){
+            boolean color[] = new boolean[5];
+            for(var c: adj.get(i)){
+                color[ans[c]]=true;
+            }
+            for(int j=1; j<=4; j++){
+                if(!color[j]){
+                    ans[i] = j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
