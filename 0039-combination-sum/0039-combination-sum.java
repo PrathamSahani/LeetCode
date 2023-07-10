918fbe8270c249d68fb9e42bf2d4b950f3f25474
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-10 12:25:13 +0530

    Time: 2 ms (81.57%), Space: 44 MB (46.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,18 +1,18 @@
 class Solution {
-   private void backtrack(int idx, int  [] arr, int target, List<List<Integer>> ans, List<Integer> ds){
-       if(idx==arr.length){
-           if(target==0){
-               ans.add(new ArrayList<>(ds));
-           }
-           return ;
-       }
-       if(arr[idx]<=target){
-           ds.add(arr[idx]);
-           backtrack(idx, arr, target-arr[idx], ans, ds);
-           ds.remove(ds.size()-1);
-       }
-       backtrack(idx+1, arr, target, ans, ds);
-   }
+    private void backtrack(int idx, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds){
+        if(idx==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }return ;
+        }
+        if(arr[idx]<=target){
+            ds.add(arr[idx]);
+            backtrack(idx, arr, target-arr[idx], ans, ds);
+            ds.remove(ds.size()-1);
+        }
+        backtrack(idx+1, arr, target, ans, ds);
+    }
+ 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(candidates);
