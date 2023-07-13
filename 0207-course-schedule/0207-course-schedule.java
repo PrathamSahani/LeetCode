c7cd989f444fbf4b8e346a2cb2e16af770af6161
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-13 10:38:03 +0530

    Time: 7 ms (41.61%), Space: 44.4 MB (36.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,33 @@
 class Solution {
     public boolean canFinish(int n, int[][] p) {
-       
-       ArrayList<Integer> topo = new ArrayList<>();
-        int indegree[] = new int[n];
-        
+        ArrayList<Integer> topo = new ArrayList<>();
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
         for(int i=0; i<n; i++){
             adj.add(new ArrayList<>());
         }
         for(int i=0; i<p.length; i++){
             adj.get(p[i][1]).add(p[i][0]);
-        }
-        for(int i=0; i<n; i++){
-            for(Integer it: adj.get(i)) {
-                indegree[it]++;
             }
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<n; i++){
-            if(indegree[i] == 0){
-                q.add(i);
+
+            int ans[] = new int[n];
+            for(int i=0; i<n; i++){
+                for(int it: adj.get(i)){
+                    ans[it]++;
+                }
             }
-        }
-      
-        while(!q.isEmpty()) {
-            Integer curr = q.poll();
-              topo.add(curr);
-            for(Integer i: adj.get(curr)) {
-                indegree[i]--;
-                if(indegree[i] == 0)
-                    q.add(i); 
+            Queue<Integer> q = new LinkedList<>();
+            for(int i=0; i<n; i++){
+                if(ans[i]==0)q.add(i);
             }
-        }
-        if(topo.size()==n)
-            return true;
-        return false;
+            while(!q.isEmpty()){
+                int node = q.poll();
+                topo.add(node);
+                for(int it: adj.get(node)){
+                    ans[it]--;
+                    if(ans[it]==0)q.add(it);
+                }
+            }
+            if(topo.size()==n)return true;
+            return false;
     }
 }
