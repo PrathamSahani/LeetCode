0ed346e8321df2978eea6c055bd8aa664137ff30
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-26 09:45:33 +0530

    Time: 7 ms (31.48%), Space: 44.7 MB (14.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,33 @@
 class Solution {
     public boolean canFinish(int n, int[][] p) {
-        ArrayList<Integer> topo = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0; i<p.length; i++){
-            adj.get(p[i][1]).add(p[i][0]);
-            }
-
-            int ans[] = new int[n];
-            for(int i=0; i<n; i++){
-                for(int it: adj.get(i)){
-                    ans[it]++;
-                }
-            }
-            Queue<Integer> q = new LinkedList<>();
-            for(int i=0; i<n; i++){
-                if(ans[i]==0)q.add(i);
-            }
-            while(!q.isEmpty()){
-                int node = q.poll();
-                topo.add(node);
-                for(int it: adj.get(node)){
-                    ans[it]--;
-                    if(ans[it]==0)q.add(it);
-                }
-            }
-            if(topo.size()==n)return true;
-            return false;
+         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+         for(int i=0; i<n; i++){
+             adj.add(new ArrayList<>());
+         }
+         for(int i=0; i<p.length; i++){
+             adj.get(p[i][1]).add(p[i][0]);
+         }
+         int ans[] = new int[n];
+         for(int i=0; i<n; i++){
+             for(int it: adj.get(i)){
+                 ans[it]++;
+             }
+         }
+         Queue<Integer> q = new LinkedList<>();
+         for(int i=0; i<n; i++){
+             if(ans[i]==0)q.add(i);
+         }
+         ArrayList<Integer> list = new ArrayList<>();
+         while(!q.isEmpty()){
+             int node = q.peek();
+             q.remove();
+             list.add(node);
+             for(int it: adj.get(node)){
+                 ans[it]--;
+                 if(ans[it]==0)q.add(it);
+             }
+         }
+         if(list.size()==n)return true;
+         return false;
     }
 }
