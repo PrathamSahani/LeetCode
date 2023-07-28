bae29e799f86db294cde68bc445e0fac434480b1
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-28 08:37:17 +0530

    Time: 5 ms (63.03%), Space: 44.4 MB (32.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,33 @@
 class Solution {
     public boolean canFinish(int n, int[][] p) {
- ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-           for(int i=0; i<n;i++) {
-               adj.add(new ArrayList<>());
-           }
-           int ans[] = new int[n];
-           for(int edge[]: p){
-               adj.get(edge[0]).add(edge[1]);
-               ans[edge[1]]++;
-           }
-           Queue<Integer> q = new LinkedList<>();
-           for(int i=0; i<n; i++){
-               if(ans[i]==0){
-                   q.add(i);
-               }
-           }
-           ArrayList<Integer> res = new ArrayList<>();
-           while(!q.isEmpty()){
-               int node = q.peek();
-               q.remove();
-               res.add(node);
-               for(int it: adj.get(node)){
-                   ans[it]--;
-                   if(ans[it]==0)q.add(it);
-               }
-           }
-            if(res.size()==n)return true;
-           return false;
+       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+       for(int i=0; i<n; i++){
+           adj.add(new ArrayList<>());
+       }   
+       int ans[] = new int[n];
+       for(int edge[]: p){
+           adj.get(edge[0]).add(edge[1]);
+           ans[edge[1]]++;
+       }
         
-       
+  
+       ArrayList<Integer> list = new ArrayList<>();
+       Queue<Integer> q = new LinkedList<>();
+       for(int i=0; i<n; i++){
+           if(ans[i]==0){
+               q.add(i);
+           }
+       }
+       while(!q.isEmpty()){
+           int node = q.peek();
+           q.remove();
+           list.add(node);
+           for(int it: adj.get(node)){
+               ans[it]--;
+               if(ans[it]==0)q.add(it);
+           }
+       }
+       if(list.size()==n)return true;
+       return false;
     }
 }
