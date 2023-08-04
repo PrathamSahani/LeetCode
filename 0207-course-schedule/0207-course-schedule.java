67aab7f98a80a67de2e5bdba330551423fe7aaeb
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-04 14:35:06 +0530

    Time: 6 ms (40.17%), Space: 44.6 MB (19.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,34 @@
 class Solution {
-    public boolean canFinish(int n, int[][] p) {
-       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-       for(int i=0; i<n; i++){
-           adj.add(new ArrayList<>());
-       }   
-       int ans[] = new int[n];
-       for(int edge[]: p){
-           adj.get(edge[0]).add(edge[1]);
-           ans[edge[1]]++;
-       }
-        
-  
-       ArrayList<Integer> list = new ArrayList<>();
-       Queue<Integer> q = new LinkedList<>();
-       for(int i=0; i<n; i++){
-           if(ans[i]==0){
-               q.add(i);
-           }
-       }
-       while(!q.isEmpty()){
-           int node = q.peek();
-           q.remove();
-           list.add(node);
-           for(int it: adj.get(node)){
-               ans[it]--;
-               if(ans[it]==0)q.add(it);
-           }
-       }
-       if(list.size()==n)return true;
-       return false;
+    public boolean canFinish(int numCourses, int[][] p) {
+          ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+          for(int i=0; i<numCourses; i++)
+          {
+              adj.add(new ArrayList<>());
+          }
+          for(int i=0; i<p.length; i++){
+              adj.get(p[i][1]).add(p[i][0]);
+          }
+          Queue<Integer> q = new LinkedList<>();
+          int ans[] = new int[numCourses];
+          for(int i=0; i<numCourses; i++){
+              for(int it: adj.get(i))
+               ans[it]++;
+          }
+          for(int i=0; i<numCourses; i++){
+              if(ans[i]==0)
+              q.add(i);
+          }
+          int count=0;
+          while(!q.isEmpty()){
+              int node = q.poll();
+             
+              count++;
+              for(int it: adj.get(node)){
+                  ans[it]--;
+                 if(ans[it]==0)q.add(it);
+              }
+          }
+          if(numCourses==count)return true;
+          return false;
     }
 }
