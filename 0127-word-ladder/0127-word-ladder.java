1930ed352848bc975926ed2ca454f6a7238c6adf
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-08 23:16:54 +0530

    Time: 59 ms (86.7%), Space: 44.9 MB (81.72%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -3,24 +3,17 @@ import java.util.*;
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
-        if (!wordSet.contains(endWord)) {
-            return 0; // End word must be in the wordList
-        }
+        if (!wordSet.contains(endWord)) return 0;
 
         Queue<String> queue = new LinkedList<>();
         queue.offer(beginWord);
-        int level = 1; // To track the number of transformations
+        int level = 1;
 
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String currentWord = queue.poll();
-
-                // Check if we reached the endWord
-                if (currentWord.equals(endWord)) {
-                    return level;
-                }
-
+                if (currentWord.equals(endWord)) return level;
                 char[] charArray = currentWord.toCharArray();
                 for (int j = 0; j < charArray.length; j++) {
                     char originalChar = charArray[j];
@@ -30,16 +23,15 @@ class Solution {
                             String transformedWord = new String(charArray);
                             if (wordSet.contains(transformedWord)) {
                                 queue.offer(transformedWord);
-                                wordSet.remove(transformedWord); // Mark as visited
+                                wordSet.remove(transformedWord);
                             }
                         }
                     }
-                    charArray[j] = originalChar; // Restore the original character
+                    charArray[j] = originalChar;
                 }
             }
-            level++; // Increment the transformation level after processing each level
+            level++;
         }
-
-        return 0; // If no transformation sequence is found
+        return 0;
     }
 }
