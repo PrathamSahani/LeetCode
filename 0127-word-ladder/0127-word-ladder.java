431a5a1a2718ee864e34636769d83e878176a664
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-17 07:33:21 +0530

    Time: 88 ms (64.47%), Space: 45.8 MB (51.05%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,36 +1,37 @@
-import java.util.*;
 
+class Pair{
+    String str;
+    int first;
+    Pair(String str, int first){
+        this.first = first;
+        this.str = str;
+    }
+}
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList);
-        if (!wordSet.contains(endWord)) return 0;
-
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord);
-        int level = 1;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                String currentWord = queue.poll();
-                if (currentWord.equals(endWord)) return level;
-                char[] charArray = currentWord.toCharArray();
-                for (int j = 0; j < charArray.length; j++) {
-                    char originalChar = charArray[j];
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        if (c != originalChar) {
-                            charArray[j] = c;
-                            String transformedWord = new String(charArray);
-                            if (wordSet.contains(transformedWord)) {
-                                queue.offer(transformedWord);
-                                wordSet.remove(transformedWord);
-                            }
-                        }
-                    }
-                    charArray[j] = originalChar;
+    public int ladderLength(String s, String e, List<String> w) {
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(s, 1));
+        Set<String> set = new HashSet<>(w);
+        set.remove(s);
+        while(!q.isEmpty()){
+            Pair it = q.poll();
+            String word = it.str;
+            int step = it.first;
+            if(word.equals(e)){
+                return step;
+            }
+            for(int i=0; i<word.length(); i++){
+                for(char ch='a'; ch<='z'; ch++){
+                char replace[] = word.toCharArray();
+                replace[i] = ch;
+                String ans = new String(replace);
+                if(set.contains(ans)){
+                    set.remove(ans);
+                    q.add(new Pair(ans, step+1));
+                }
                 }
             }
-            level++;
         }
         return 0;
     }
