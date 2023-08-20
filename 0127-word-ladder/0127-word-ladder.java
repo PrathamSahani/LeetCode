a7600bc45172ae1fa34f734a30e2e4fa24b4b241
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-20 13:36:07 +0530

    Time: 94 ms (56.99%), Space: 46.2 MB (42.2%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,38 +1,39 @@
-
 class Pair{
     String str;
     int first;
     Pair(String str, int first){
-        this.first = first;
         this.str = str;
+        this.first = first;
     }
 }
 class Solution {
     public int ladderLength(String s, String e, List<String> w) {
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(s, 1));
-        Set<String> set = new HashSet<>(w);
-        set.remove(s);
-        while(!q.isEmpty()){
-            Pair it = q.poll();
-            String word = it.str;
-            int step = it.first;
-            if(word.equals(e)){
-                return step;
-            }
-            for(int i=0; i<word.length(); i++){
-                for(char ch='a'; ch<='z'; ch++){
-                char replace[] = word.toCharArray();
-                replace[i] = ch;
-                String ans = new String(replace);
-                if(set.contains(ans)){
-                    set.remove(ans);
-                    q.add(new Pair(ans, step+1));
+            Queue<Pair> q = new LinkedList<>();
+            q.add(new Pair(s, 1));
+            HashSet<String> set = new HashSet<>(w);
+            set.remove(s);
+            while(!q.isEmpty()){
+                Pair it = q.poll();
+                String word = it.str;
+                int step = it.first;
+                if(word.equals(e)){
+                    return step;
                 }
+                for(int i=0; i<word.length(); i++){
+                    for(char ch='a'; ch<='z'; ch++){
+                         if(ch==word.charAt(i))
+                         continue;
+                         char  replace[] = word.toCharArray();
+                         replace[i] = ch;
+                         String rep = new String(replace);
+                         if(set.contains(rep)){
+                             set.remove(rep);
+                             q.add(new Pair(rep, step+1));
+                         }
+                    }
                 }
+            
             }
-        }
-        return 0;
+            return 0;
     }
 }
