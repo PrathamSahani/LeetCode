85e721b4f2c597d1966d46e7470478d53e9c2ab8
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-10-25 21:05:02 +0530

    Time: 27 ms (25.42%), Space: 55.3 MB (9.64%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,34 +1,37 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int V = graph.length;
-         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<V; i++){
+       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int n = graph.length;
+        for(int i=0; i<n; i++){
             adj.add(new ArrayList<>());
         }
-        int indgree[] = new int[V];
-        for(int i=0; i<V; i++){
+        int ans[] = new int[n];
+        for(int i=0; i<n; i++){
             for(int it: graph[i]){
                 adj.get(it).add(i);
-                indgree[i]++;
-            }
+                ans[i]++;
+            }   
         }
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> safe = new ArrayList<>();
-        for( int i=0; i<V; i++){
-            if(indgree[i]==0){
+         Queue<Integer> q = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(ans[i]==0){
                 q.add(i);
             }
         }
+       
         while(!q.isEmpty()){
             int node = q.peek();
             q.remove();
-            safe.add(node);
-            for(int it:adj.get(node)){
-                indgree[it]--;
-                if(indgree[it]==0)q.add(it);
+            list.add(node);
+            for(int it: adj.get(node)){
+                ans[it]--;
+                if(ans[it]==0){
+                    q.add(it);
+                }
             }
         }
-        Collections.sort(safe);
-        return safe;
+        Collections.sort(list);
+        return list;
     }
 }
