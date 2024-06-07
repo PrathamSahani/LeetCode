1f6a18c4676663522f47a29fd3ef3147d3e2cf59
Pratham Sahani <106865923+PrathamSahani@users.noreply.github.com>
2024-06-07 12:11:26 +0530

    Time: 0 ms (100%), Space: 42.3 MB (18.95%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-            //Transpose
-       for(int i=0; i<matrix.length; i++){
-           for(int j=i; j<matrix[0].length; j++){
-               int temp = matrix[i][j];
-               matrix[i][j] = matrix[j][i];
-               matrix[j][i] = temp;
-           }
-       }
-       for(int i=0; i<matrix.length; i++){
-         
-               int right = 0, left = matrix.length-1;
-               while(right<=left){
-                   int temp = matrix[i][right];
-                   matrix[i][right] = matrix[i][left];
-                   matrix[i][left] = temp;
-                   left--; right++;
-               }
-           
-       }
+          int n = matrix.length, m = matrix[0].length;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<m; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0; i<matrix.length; i++){
+            int left =0, right = matrix.length-1;
+            while(left<=right){
+                int temp = matrix[i][right];
+                matrix[i][right] = matrix[i][left];
+                matrix[i][left] = temp;
+                left++;right--;
+            }
+        }
     }
 }
